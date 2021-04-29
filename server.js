749412b6da8f888019e78c723bb22cd7163a2f4f
Tomas Pinto <tpinto@atixlabs.com>
2021-04-29 11:23:30 -0300

    handle invalid urls

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const validUrl = require('valid-url');
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -18,6 +19,20 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.route("/api/shorturl")
+    .get(function(req, res) {
+        //TO DO
+})
+    .post(function(req, res) {
+        console.log(req.body);
+        console.log(req.body.url);
+        if(!validUrl.isUri(req.body.url)) {
+          res.json({"error": "Invalid URL"});
+          return;
+        }
+        res.json({"OK": "Valid URL"});
+    });
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
